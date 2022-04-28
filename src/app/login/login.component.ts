4d126c7fe8318e10f6d48983f80af479866122bf
raeez nazar <raeeznazar>
2022-04-28 18:02:59 +0530

    one way binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,13 +3,56 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  aim = 'Your perfect banking partner';
+  placeHold = 'account number please';
+  acno = '';
+  pswd = '';
 
-  constructor() { }
+  //database
+  database: any = {
+    1000: { acno: 1000, uname: 'raeez', password: 1000, balance: 50000 },
+    1001: { acno: 1001, uname: 'florie', password: 1001, balance: 10000 },
+    1002: { acno: 1002, uname: 'Vyom', password: 1002, balance: 5000 },
+  };
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // login() {
+  //   var acno = this.acno;
+  //   var pswd = this.pswd;
+
+  //   let database = this.database;
+
+  //   if (acno in database) {
+  //     if (pswd == database[acno]['password']) {
+  //       alert('login sucessfully');
+  //     } else {
+  //       alert('invalid password');
+  //     }
+  //   } else {
+  //     alert('user does not exist');
+  //   }
+  // }
+
+  // login using template referencing
+  login(a: any, p: any) {
+    var acno = a.value;
+    var pswd = p.value;
+
+    let database = this.database;
+
+    if (acno in database) {
+      if (pswd == database[acno]['password']) {
+        alert('login sucessfully');
+      } else {
+        alert('invalid password');
+      }
+    } else {
+      alert('user does not exist');
+    }
   }
-
 }
